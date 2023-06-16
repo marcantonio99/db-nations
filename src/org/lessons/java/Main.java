5554c0f810c233ad1b40c8e334d5da9eaa735da8
marcantonio99 <marcantonio8799@gmail.com>
2023-06-16 17:08:37 +0200

    Milestone 3

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,9 +1,7 @@
 package org.lessons.java;
 
-import com.sun.source.tree.TryTree;
-
-import javax.xml.transform.Result;
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,12 +12,22 @@ public class Main {
         try(Connection connection = DriverManager.getConnection(url, user, password)){
             System.out.println(connection.getCatalog());
 
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Inserisci la stringa di ricerca: ");
+            String searchString = scanner.nextLine();
+
             String sql = "SELECT n.name AS Nazione, n.country_id AS ID_Nazione, r.name AS Regione, c.name AS Continente\n" +
                     "FROM countries n\n" +
                     "INNER JOIN regions r ON n.region_id = r.region_id\n" +
                     "INNER JOIN continents c ON r.continent_id = c.continent_id\n" +
+                    "WHERE n.name LIKE ? OR r.name LIKE ? OR c.name LIKE ?\n" +
                     "ORDER BY n.name;";
+
             try (PreparedStatement ps = connection.prepareStatement(sql)){
+                ps.setString(1, "%" + searchString + "%");
+                ps.setString(2, "%" + searchString + "%");
+                ps.setString(3, "%" + searchString + "%");
+
                try (ResultSet rs = ps.executeQuery()){
                    while (rs.next()){
                        String nomeNazione = rs.getString("Nazione");
